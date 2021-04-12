c0151caeb619b0608b97f8576b362836ea74abc9
Jay Patel <jd4215124@gmail.com>
2021-04-12 22:33:35 +0530

    first commit

---
+++ src/layout/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-info text-center pt-3 pb-2 mt-5 fixed-bottom text-white">
+      &copy; &nbsp;
+      <a href="https://github.com/jaypatel1210" target="_blank" style={{ color: 'white' }}>
+        Jay Patel
+      </a>
+    </footer>
+  );
+};
+export default Footer;
---
+++ src/utils/config.js
@@ -0,0 +1,19 @@
+//TODO: add firebase configuration and export it
+
+//image configuration
+export const imageConfig = {
+  quality: 0.2,
+  maxWidth: 800,
+  maxHeight: 600,
+  autoRotate: true
+};
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyABT5PsOk38qXJwAyDhoT-mK26062Dac34',
+  authDomain: 'react-contact-app-27c43.firebaseapp.com',
+  projectId: 'react-contact-app-27c43',
+  storageBucket: 'react-contact-app-27c43.appspot.com',
+  messagingSenderId: '112793338951',
+  appId: '1:112793338951:web:69aa944055e23b53615d62',
+  measurementId: 'G-FFT0G242ZQ'
+};
